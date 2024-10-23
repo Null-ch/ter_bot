a7010e528e1e383e4baf0f6af9c85fea08354d89
Null-ch <zuxobot@yandex.ru>
2024-10-23 13:45:04 +0300

    upd

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/webhook', [TelegramController::class, 'handleWebhook'])->name('telegram_webhook')->middleware(['web', 'except:web']);
+Route::post('/webhook', [TelegramController::class, 'handleWebhook'])->name('telegram_webhook');
 Route::get('/webhook/set', [TelegramController::class, 'setWebhook']);
